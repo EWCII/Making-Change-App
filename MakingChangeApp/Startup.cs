dc4ac268c2ce90f8250a13450fc88c44ad5ba9aa
Carrington Eugene <eugenecarringtonii@icloud.com>
2018-06-16 18:41:35 -0500

    Add project files.

---
+++ MakingChangeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MakingChangeApp.Startup))]
+namespace MakingChangeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
